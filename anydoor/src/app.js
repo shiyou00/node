2322b53de618602527171abf148ad3c6120ff7d2
shiyou <8533334@qq.com>
2019-07-05 11:21:22 +0800

    add anydoor serve

--- anydoor/src/app.js
+++ anydoor/src/app.js
@@ -1,2 +1,15 @@
 const http = require('http');
 const conf = require('./config/defaultConfig');
+const chalk = require('chalk');
+
+
+const server = http.createServer((req,res)=>{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world');
+})
+
+server.listen(conf.port,conf.host,() => {
+  const addr = `http://${conf.host}:${conf.port}`;
+  console.log(`Server started at ${addr}`);
+})
--- anydoor/src/config/defaultConfig.js
+++ anydoor/src/config/defaultConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-  port: '9527',
+  port: 9527,
   host: '127.0.0.1'
 };
