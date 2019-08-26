34a179e9f7ec69b59aa6d26182139ac64d34f8af
shiyou <8533334@qq.com>
2019-08-26 17:25:05 +0800

    add pritter

--- anydoor/.eslintrc.js
+++ anydoor/.eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-      "no-console":["error",{
-        "allow": ["warn", "error", "info"]
-      }]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "error", "info"]
+      }
+    ]
+  }
 };
--- anydoor/src/app.js
+++ anydoor/src/app.js
@@ -1,16 +1,19 @@
-const http = require('http');
-const conf = require('./config/defaultConfig');
+const http = require("http");
+const conf = require("./config/defaultConfig");
 // const chalk = require('chalk');
 
-
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hello world');
-})
+  res.setHeader("Content-Type", "text/plain");
+  res.end("hello world");
+});
 
-server.listen(conf.port,conf.host,() => {
+server.listen(conf.port, conf.host, () => {
   const addr = `http://${conf.host}:${conf.port}`;
   console.info(`Server started at ${addr}`);
-})
+});
 
+const app = {
+  a: 4,
+  b: 5
+};
